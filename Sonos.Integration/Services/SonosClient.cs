9d28ccdf1235b64b9def1e6066b5156ca6f179b8
Collins Mugarura <collismugy@hotmail.com>
2018-09-09 16:32:30 +0100

    Add project files.

---
+++ Sonos.Integration/Services/SonosClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sonos.Integration.Services
+{
+    public class SonosClient : ISonosClient
+    {
+        public void ConnectToSonos()
+        {
+            try
+            {
+                using (var client = new HttpClient(new HttpClientHandler()))
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
